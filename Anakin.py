3ef9691c8437280b801a88f706aedafd62083df3
Mustafa Kum <86945272+Mustafa-Kum@users.noreply.github.com>
2022-10-01 16:18:26 +0300

    Add files via upload

---
+++ Anakin.py
@@ -0,0 +1,3 @@
+import Yoda 
+
+print("Anakin")
---
+++ Yoda.py
@@ -0,0 +1,12 @@
+def func_direct():
+    print("Yoda Direct")
+
+def func_imported():
+    print("Yoda İmported")
+
+if __name__ == "__main__":
+    func_direct()
+
+else:
+    func_imported()
+
